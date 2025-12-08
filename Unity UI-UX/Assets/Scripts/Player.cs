71972a635f9876fd4cdd70a82d9fdf6c2f14e92d
Noa <nducrot@gaming.tech>
2025-12-08 10:49:02 +0100

    rotation joueur modifié

--- Unity UI-UX/Assets/Scripts/Player.cs
+++ Unity UI-UX/Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Windows;
 using static UnityEngine.InputSystem.InputAction;
 
@@ -7,17 +8,17 @@ public class Player : MonoBehaviour
 
     [Header("Movement")]
     [SerializeField] private float speed = 5f;
-    [SerializeField] private float rotationSpeed = 5f;
+    [SerializeField] private float rotateSpeed = 5f;
     [SerializeField] private Rigidbody rigidBody = null;
-    [SerializeField] private Vector2 minMaxYaw = new(-90f, 90f);
+    private float rotateAmount = 90;
 
     [Header("Transforms")]
     [SerializeField] private Transform root = null;
     [SerializeField] private Transform head = null;
 
     private Vector3 input = Vector3.zero;
-    private Vector2 rotationInput;
-    private Vector2 currentRotation;
+    private float targetYaw;
+    private float currentYaw;
 
     private void Reset()
     {
@@ -32,20 +33,22 @@ public class Player : MonoBehaviour
         input.y = 0;
     }
 
-    public void Player_OnLook(CallbackContext context)
+    public void Player_OnLook(InputAction.CallbackContext context)
     {
-        rotationInput = context.ReadValue<Vector2>();
-    }
+        if (!context.performed)
+            return;
 
+        Vector2 input = context.ReadValue<Vector2>(); 
+        float x = input.x;
+
+        if (x != 0)
+            targetYaw += x * rotateAmount;
+    }
 
     private void LateUpdate()
     {
-        currentRotation.x += -rotationInput.y * rotationSpeed * Time.deltaTime;
-        currentRotation.y += rotationInput.x * rotationSpeed * Time.deltaTime;
-        currentRotation.x = Mathf.Clamp(currentRotation.x, minMaxYaw.x, minMaxYaw.y);
-
-        root.localRotation = Quaternion.Euler(0, currentRotation.y, 0);
-        head.localRotation = Quaternion.Euler(currentRotation.x, 0, 0);
+        currentYaw = Mathf.Lerp(currentYaw, targetYaw, Time.deltaTime * rotateSpeed);
+        root.localRotation = Quaternion.Euler(0f, currentYaw, 0f);
     }
 
     private void FixedUpdate()
